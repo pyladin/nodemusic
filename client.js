f3c30a3b58fbbfe7e919c9b685127eccfd577eb5
Nico Bressler <nbressler@ci.lebanon.or.us>
2019-09-16 15:42:22 -0700

    attempting to fix client reconnect issue

--- client.js
+++ client.js
@@ -8,7 +8,7 @@ var dotenv = require('dotenv'); // Allows us to use an env file to store importa
 dotenv.config();
 
 // Connect to our socket.io server after initializing the environment variables
-var socket = io.connect('http://' + process.env.SERVER_ADDRESS + ':' + process.env.SERVER_PORT, { forceNew: true });
+var socket = io.connect('http://' + process.env.SERVER_ADDRESS + ':' + process.env.SERVER_PORT, { reconnection: true, reconnectionDelay: 1000, reconnectionDelayMax : 5000, reconnectionAttempts: Infinity });
 
 // Connect to the server and write to the console that we've connected
 socket.on('connect', function() {
