0067a2eb12d7257e03459e0f7f4c127cda912560
Nico Bressler <nbressler@ci.lebanon.or.us>
2019-09-17 13:21:01 -0700

    working through some bugs

--- client.js
+++ client.js
@@ -11,4 +11,12 @@ var socket = io('http://' + process.env.SERVER_ADDRESS + ':' + process.env.SERVE
 socket.on('connect', function() {
   // Log to the console when we have successfully connected to the socket.io server
   console.log('A connection to the server has been made.');
+
+  socket.on('request-details', function(callback) {
+    var clientDetails = {
+      clientName: process.env.CLIENT_NAME,
+      clientLocation: process.env.CLIENT_LOCATION
+    };
+    callback(null, clientDetails);
+  });
 });
