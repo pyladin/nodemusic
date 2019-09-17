3c282f932321c8c70676657f41168885cd866d89
Nico Bressler <nbressler@ci.lebanon.or.us>
2019-09-17 10:52:46 -0700

    working through some bugs

--- client.js
+++ client.js
@@ -1,13 +1,19 @@
-var io = require('socket.io-client');
-var dotenv = require('dotenv');
+var io = require('socket.io-client'); // Allows us to connect to the socket.io server as a client without a browser
+var dotenv = require('dotenv'); // Allows us to use an env file to store important variables
 
+// Initialize dotenv
 dotenv.config();
 
+// Connect to our socket.io server after initializing the environment variables
 var socket = io('http://' + process.env.SERVER_ADDRESS + ':' + process.env.SERVER_PORT);
 
+// Listen for a successfull connection to the socket.io server
 socket.on('connect', function() {
+  // Log to the console when we have successfully connected to the socket.io server
   console.log('A connection to the server has been made.');
 
+  // Listen for the "request-client-details" event and then emit a
+  // "send-client-details" event back to the server
   socket.on('request-client-details', function() {
     socket.emit('send-client-details', { clientName: process.env.CLIENT_NAME, clientLocation: process.env.CLIENT_LOCATION });
   });
--- server.js
+++ server.js
@@ -1,27 +1,39 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var app = require('express')(); // Allows us to serve up http pages easily
+var http = require('http').createServer(app); // Creates our http server
+var io = require('socket.io')(http); // Creates our socket.io server
 
+// Send the home page when get request made to root directory
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+// Listen for connection events from clients
 io.on('connection', function(socket) {
+  // Log to the console that a client has connected
   console.log('A client has connected');
 
-  socket.on('ask-client-details', function() {
-    io.emit('request-client-details');
-  });
+  // Listen for the "ask-client-details" event and then emit a
+  // "request-client-details" event to all the clients.
+  // socket.on('ask-client-details', function() {
+  //   io.emit('request-client-details');
+  // });
+  socket.on('ask-client-details', 'Yes!');
 
+  // Listen for the "send-client-details" event and then emit a
+  // "update client-details" event to all the clients
   socket.on('send-client-details', function(data) {
     io.emit('update-client-details', {clientName: data.clientName, clientLocation: data.clientLocation });
   });
 
+  // Listen for disconnect events from clients
   socket.on('disconnect', function() {
+    // Log to the console that a client has disconnected
     console.log('A client has disconnected');
   });
 });
 
+// Start our http server on port 8080
 http.listen(8080, function() {
+  // Log to the console that we are listening
   console.log('listening on *:8080');
 });
