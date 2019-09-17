a92b050d646cf55d70387caefc94f35b52ea130e
Nico Bressler <nbressler@ci.lebanon.or.us>
2019-09-17 11:57:37 -0700

    working through some bugs

--- client.js
+++ client.js
@@ -1,8 +1,5 @@
 var io = require('socket.io-client'); // Allows us to connect to the socket.io server as a client without a browser
 var dotenv = require('dotenv'); // Allows us to use an env file to store important variables
-var { spawn } = require('child_process'); // // Allows us to spawn ffmpeg and ffplay processes
-var fs = require('fs'); // Allows us to read and write files
-var find = require('find-process'); // Allows us to check if the ffmpeg and ffplay processes area already running
 
 // Initialize dotenv
 dotenv.config();
@@ -14,50 +11,4 @@ var socket = io('http://' + process.env.SERVER_ADDRESS + ':' + process.env.SERVE
 socket.on('connect', function() {
   // Log to the console when we have successfully connected to the socket.io server
   console.log('A connection to the server has been made.');
-
-  // Listen for the "start-ffplay" event and then start ffplay
-  socket.on('start-ffplay', function(data) {
-    find('name', 'ffplay', true)
-    .then(function (list) {
-      if(!list.length) {
-        // If ffplay is not running
-        // Build the client sdp file from the master sdp file sent from the server
-        fs.writeFile('client.sdp', data.sdpFile, function(err) {
-          if(err) {
-            console.log(err);
-          };
-          console.log('SDP file created successfully!');
-        });
-
-        // Start ffplay and store the process in a variable so we can do things to it
-        var ffplayCmd = spawn('ffplay', [data.ffplayFlags.sdpFile, data.ffplayFlags.protocolWhitelist, data.ffplayFlags.reorderQueueSize], { shell: true });
-
-        //
-        ffplayCmd.stdout.on('data', (data) => {
-          console.log(`stdout: ${data}`);
-        });
-
-        ffplayCmd.stderr.on('data', (data) => {
-          console.error(`stderr: ${data}`);
-        });
-
-        // Write to the console to notify that ffplay is started
-        console.log('ffplay has started');
-      } else {
-        // If ffplay is already running
-        console.log('ffplay is already running');
-      };
-    });
-  });
-
-  // Listen for the "request-client-details" event and then emit a
-  // "send-client-details" event back to the server
-  socket.on('request-client-details', function() {
-    clientDetails = {
-      clientName: process.env.CLIENT_NAME,
-      clientLocation: process.env.CLIENT_LOCATION,
-      clientID: socket.id
-    };
-    socket.emit('send-client-details', { clientDetails })
-  });
 });
--- server.js
+++ server.js
@@ -2,49 +2,9 @@ var app = require('express')(); // Allows us to serve up http pages easily
 var http = require('http').createServer(app); // Creates our http server
 var io = require('socket.io')(http); // Creates our socket.io server
 var dotenv = require('dotenv'); // Allows us to use an env file to store important variables
-var { spawn } = require('child_process'); // Allows us to spawn ffmpeg and ffplay processes
-var fs = require('fs'); // Allows us to read and write files
-var find = require('find-process'); // Allows us to check if the ffmpeg and ffplay processes area already running
 
 // Initialize dotenv
 dotenv.config();
-
-// Find if ffmpeg is already started and don't start another one
-find('name', 'ffmpeg', true)
-.then(function (list) {
-  if(!list.length) {
-    // If ffmpeg is not running
-    // Start ffmpeg and store the process in a variable so we can do things to it
-    var ffmpegCmd = spawn('ffmpeg', [`-i ${process.env.STREAM_SOURCE}`, `-acodec ${process.env.AUDIO_CODEC}`, `-f ${process.env.BROADCAST_FORMAT}`, `rtp://${process.env.BROADCAST_ADDRESS}:${process.env.BROADCAST_PORT}`], { shell: true });
-
-    // Write stdout to the console
-    ffmpegCmd.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
-    });
-
-    // Write stderr to the console
-    ffmpegCmd.stderr.on('data', (data) => {
-      console.error(`stderr: ${data}`);
-    });
-  } else {
-    // If ffmpeg is already running
-    console.log('ffmpeg is already running');
-  };
-});
-
-// Set the flags that we want our clients to use when calling ffplay
-var ffplayFlags = {
-  sdpFile: '-i ' + process.env.SDP_FILE, // Tells the client where the SDP file is located
-  protocolWhitelist: '-protocol_whitelist ' + process.env.PROTOCOL_WHITELIST, // Ffplay requires us to whitelist protocols
-  reorderQueueSize: '-reorder_queue_size ' + process.env.REORDER_QUEUE_SIZE // Undocumented flag to help with restarting/latency
-};
-
-// Read our master SDP file into a varialbe to be sent to the client
-try {
-  var masterSDP = fs.readFileSync('master.sdp', 'utf8');
-} catch(err) {
-  console.log('Error:', err.stack);
-}
 
 // Send the home page when get request made to root directory
 app.get('/', function(req, res) {
@@ -55,23 +15,6 @@ app.get('/', function(req, res) {
 io.on('connection', function(socket) {
   // Log to the console that a client has connected
   console.log('A client has connected');
-
-  // Send the "start-ffplay" event every second to make sure that ffplay is started and running
-  setInterval(() => {
-    socket.emit('start-ffplay', { ffplayFlags: ffplayFlags, sdpFile: masterSDP });
-  }, 1000);
-
-  // Listen for the "ask-client-details" event and then emit a
-  // "request-client-details" event to all the clients.
-  socket.on('ask-client-details', function() {
-    io.emit('request-client-details');
-  });
-
-  // Listen for the "send-client-details" event and then emit a
-  // "update client-details" event to all the clients
-  socket.on('send-client-details', function(clientDetails) {
-    io.emit('update-client-details', { clientDetails });
-  });
 
   // Listen for disconnect events from clients
   socket.on('disconnect', function() {
