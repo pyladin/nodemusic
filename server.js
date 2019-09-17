a8769418c9eca602d77baaea9050bb4f4df203be
Nico Bressler <nbressler@ci.lebanon.or.us>
2019-09-17 09:59:00 -0700

    working through some bugs

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  console.log('A client has connected');
 });
 
 http.listen(8080, function() {
